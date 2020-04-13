e3340644e7a6f01d07c0be572ec7b34bdd29a652
Pmparekh2000 <pmp.tech2000@gmail.com>
2020-04-13 23:45:53 +0530

    Form validation added and service made for adding new item

---
+++ src/shared/models/budget.item.model.ts
@@ -0,0 +1,15 @@
+// export class BudgetItem{
+//     description: string;
+//     amount: number;
+
+//     constructor(description: string, amount: number){
+//         this.description = description;
+//         this.amount = amount;
+//     }
+// }
+
+// Cut short version
+
+export class BudgetItem{
+    constructor(public description: string, public amount: number) {}
+}
